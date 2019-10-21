d34cecdc323e5653b5fd2267087cd3b252d75e01
beginor <beginor@qq.com>
2019-10-21 15:24:20 +0800

    texture test.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,10 @@
 import {
     Scene, PerspectiveCamera, WebGLRenderer, BoxGeometry,
-    MeshNormalMaterial, Mesh, MeshBasicMaterial, TextureLoader, MeshStandardMaterial, MeshPhongMaterial
+    MeshNormalMaterial, Mesh, MeshBasicMaterial, TextureLoader,
+    MeshStandardMaterial, MeshPhongMaterial, Color, PlaneBufferGeometry,
+    DoubleSide
 } from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 export class App {
 
@@ -9,40 +12,62 @@ export class App {
     camera: PerspectiveCamera;
     renderer: WebGLRenderer;
     cube: Mesh;
+    controls: OrbitControls;
 
     constructor(private container: HTMLElement) {
     }
 
     private init(): void {
         this.scene = new Scene();
+        this.scene.background = new Color("#FFFF00");
         this.camera = new PerspectiveCamera(
             75,
             this.container.clientWidth / this.container.clientHeight,
             0.1,
             1000
         );
-        this.renderer = new WebGLRenderer({ antialias: false, alpha: true });
+        this.renderer = new WebGLRenderer({
+            antialias: false, alpha: true
+        });
+        this.renderer.autoClear = false;
+        // this.renderer.autoClearColor = false;
+        // this.renderer.autoClearDepth = false;
+        // this.renderer.autoClearStencil = false;
         this.renderer.setSize(
             this.container.clientWidth,
             this.container.clientHeight
         )
         this.container.appendChild(this.renderer.domElement);
         var texture = new TextureLoader().load('./happy_santaclaus.png');
-        var geometry = new BoxGeometry(1, 1, 1);
+        var geometry = new BoxGeometry(1, 1, 1, 16, 16, 16);
         var material = new MeshBasicMaterial({
             map: texture,
             opacity: 0.9,
-            transparent: true
+            transparent: true,
+            wireframe: false
         });
         this.cube = new Mesh(geometry, material);
-        this.scene.add(this.cube);
+        // this.scene.add(this.cube);
+
+        const planeGeo = new PlaneBufferGeometry(5, 5, 16, 16);
+        const planeMat = new MeshBasicMaterial({
+            // color: 0xff0000,
+            side: DoubleSide,
+            map: texture,
+            transparent: true,
+            opacity: 0.3
+        });
+        const plane = new Mesh(planeGeo, planeMat);
+        this.scene.add(plane);
 
         this.camera.position.z = 5;
+        this.controls = new OrbitControls(this.camera, this.container);
+        window['scene'] = this.scene;
     }
 
     private update(time: number): void {
-        this.cube.rotation.x += 0.01;
-        this.cube.rotation.y += 0.02;
+        // this.cube.rotation.x += 0.01;
+        // this.cube.rotation.y += 0.02;
     }
 
     private animate(time: number): void {
